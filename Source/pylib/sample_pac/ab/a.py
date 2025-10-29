6beb9c16affb28d989f62c2ebc0ce92ae823c695
Blueblueowl <nam.seungsuk@gmail.com>
2025-10-29 18:00:45 +0900

    css속성

--- Source/pylib/sample_pac/ab/a.py
+++ Source/pylib/sample_pac/ab/a.py
@@ -1,5 +1,6 @@
 # sample_pac/ab/a.py
+
 def hello():
-    print("sample_pac.ab.a모듈의 hello")
-if __name__ == "__main__":
+    print('sample_pac.ab.a모듈의 hello')
+if __name__ == '__main__':
     hello()
--- Source/pylib/sample_pac/ab/b.py
+++ Source/pylib/sample_pac/ab/b.py
@@ -1,4 +1,6 @@
+# sample_pac/ab/b.py (python sample_pac/ab/b.py)
+
 def world():
-    print("sample_pac.ac.b모듈의 world")
-if __name__ == "__main__":
+    print('sample_pac.ab.b모듈의 world')
+if __name__ == '__main__':
     world()
