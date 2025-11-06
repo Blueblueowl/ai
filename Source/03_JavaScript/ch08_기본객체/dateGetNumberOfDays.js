4f9bc0c791b7385077af78b73da96999b53948c3
Blueblueowl <nam.seungsuk@gmail.com>
2025-11-06 18:00:16 +0900

    객체와이벤트

--- Source/03_JavaScript/ch08_기본객체/dateGetNumberOfDays.js
+++ Source/03_JavaScript/ch08_기본객체/dateGetNumberOfDays.js
@@ -1,12 +1,12 @@
 // dateGetNumberOfDays.js
 // now.getNumberOfDays(openday) => now는this/ openday는 that
 Date.prototype.getNumberOfDays = function(that){
-  let intervalMilisec = Math.abs(this.getTime() - that.getTime()); // 두 날짜의 차이를 밀리초 단위로 계산
-  let day = Math.floor(intervalMilisec/(1000 * 60 * 60 * 24)); // 밀리초를 일 단위로 변환
-  return day;  
+  let intervalMilisec = Math.abs(this.getTime() - that.getTime()); // 절대값
+  let day = Math.floor(intervalMilisec/(1000*60*60*24)); // 내림
+  return day;
 };
-let now = new Date();
-let limitday = new Date(2026, 1, 12, 18, 0, 0); // 2026년 2월 12일 18시 0분 0초
-console.log(now.getNumberOfDays(limitday), '일 남음');
-console.log(limitday.getNumberOfDays(now), '일 지남');
-console.log(now.getNumberOfDays(limitday), '일 남음');
+// let now = new Date();
+// let limitday = new Date(2026, 1, 12, 18, 0, 0); // 26.2.12
+// console.log(now.getNumberOfDays(limitday), '일 남음');
+// console.log(limitday.getNumberOfDays(now), '일 남음' );
+// console.log(now.getNumberOfDays(now));
